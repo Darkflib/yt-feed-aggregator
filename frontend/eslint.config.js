d79d0456a2a3aa03ef16e2b195b6c2b26a7ec667
Mike <darkflib@gmail.com>
2025-11-06 03:34:03 +0000

    chore(frontend): migrate ESLint to flat config (eslint.config.js), remove .eslintrc.cjs; add @eslint/js and simplify lint script

---
+++ frontend/eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js'
+import typescript from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: {
+        browser: true,
+        es2020: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...typescript.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+]
